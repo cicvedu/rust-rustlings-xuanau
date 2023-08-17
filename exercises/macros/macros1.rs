e4e2079f804d9e0d48fd188d96ffb629765b3dfa
xuanau <95044645+xuanau@users.noreply.github.com>
2023-08-17 11:20:48 +0800

    Update macros1.rs

--- exercises/macros/macros1.rs
+++ exercises/macros/macros1.rs
@@ -2,14 +2,14 @@
 //
 // Execute `rustlings hint macros1` or use the `hint` watch subcommand for a
 // hint.
-
+#[macro_export]
 macro_rules! my_macro {
     () => {
         println!("Check out my macro!");
     };
 }
 
-#[macro_export]
+
 fn main() {
     my_macro!();
 }
