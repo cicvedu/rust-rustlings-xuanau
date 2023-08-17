930836de4679b2f1ea2c3a40af2de2ba4f4be7cb
xuanau <95044645+xuanau@users.noreply.github.com>
2023-08-17 20:59:32 +0800

    Update drive4.rs

--- exercises/drive/drive4.rs
+++ exercises/drive/drive4.rs
@@ -3,13 +3,24 @@
 // Execute `rustlings hint drive1` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
 
 // This execrise shares build.rs with the previous exercise.
 // You need to add some code to build.rs to make both this exercise and
 // the previous one work.
+use std::env;
+use std::time::{SystemTime,UNIX_EPOCH};
+
 
 fn main() {
+    let tempstamp = SystemTime::now().duration_since(UNIX_EPOCH).expect("Time went backwards").as_secs();
+    let test_foo_value = timestamp+5;
+    env::set_var("TEST_FOO",test_foo_value.to_string());
+    println!("cargo:return-if-changed=build.rs");
+    println!("rcargo:zustc-env=TEST_FOO={}",test_foo_value);
+    if test_foo_value %2 ==0{
+        println!("cargo:rustc-cfg=feature=\"pass\"");
+        
+    }
 
 }
 
