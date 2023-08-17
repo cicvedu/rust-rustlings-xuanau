0312a043a274af871a38ce9e5a058b18b9a53d1e
xuanau <95044645+xuanau@users.noreply.github.com>
2023-08-17 17:21:21 +0800

    Update clippy3.rs

--- exercises/clippy/clippy3.rs
+++ exercises/clippy/clippy3.rs
@@ -5,11 +5,13 @@
 // Execute `rustlings hint clippy3` or use the `hint` watch subcommand for a hint.
 
 
+
 #[allow(unused_variables, unused_assignments)]
 fn main() {
     let my_option: Option<()> = None;
-    my_option.unwrap();
-
+    if my_option.is_none() {
+        //my_option.unwrap();
+    }
 
     let my_arr = &[
         -1, -2, -3,
@@ -22,9 +24,10 @@ fn main() {
 
     let mut value_a = 45;
     let mut value_b = 66;
+    let mut value_temp = 0;
     // Let's swap these two!
-    //value_a = value_b;
-    //value_b = value_a;
-    std::mem::swap(&mut value_a,&mut value_b);
+    value_temp = value_b;
+    value_b = value_a;
+    value_a = value_temp;
     println!("value a: {}; value b: {}", value_a, value_b);
 }
