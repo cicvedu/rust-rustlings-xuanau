d9c35fa739a8681c8dfe808a76965b661c725508
xuanau <95044645+xuanau@users.noreply.github.com>
2023-08-17 17:23:38 +0800

    Update from_into.rs

--- exercises/conversions/from_into.rs
+++ exercises/conversions/from_into.rs
@@ -40,6 +40,7 @@ impl Default for Person {
 // If while parsing the age, something goes wrong, then return the default of
 // Person Otherwise, then return an instantiated Person object with the results
 
+
 impl From<&str> for Person {
     fn from(s: &str) -> Person {
         let mut x:Person =  Person::default();
@@ -73,6 +74,12 @@ impl From<&str> for Person {
         x
     }
 }
+
+// impl From<&str> for Person {
+//     fn from(s: &str) -> Person {
+//     }
+// }
+
 fn main() {
     // Use the `from` function
     let p1 = Person::from("Mark,20");
@@ -153,14 +160,14 @@ mod tests {
     #[test]
     fn test_trailing_comma() {
         let p: Person = Person::from("Mike,32,");
-        assert_eq!(p.name, "John");
-        assert_eq!(p.age, 30);
+        assert_eq!(p.name, "Mike");
+        assert_eq!(p.age, 32);
     }
 
     #[test]
     fn test_trailing_comma_and_some_string() {
         let p: Person = Person::from("Mike,32,man");
-        assert_eq!(p.name, "John");
-        assert_eq!(p.age, 30);
+        assert_eq!(p.name, "Mike");
+        assert_eq!(p.age, 32);
     }
 }
